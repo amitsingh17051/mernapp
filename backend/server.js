9ad7069c227b8842618456eaea58786039d1d319
unknown <amitsingh17051@gmail.com>
2022-02-09 18:04:27 +0530

    Added get request route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+
+
+const port = process.env.PORT || 5000
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.json({
+		message: "This is a get request",
+	})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
+
